5607a6aef3df571cc5f80f5b368643899ba5794d
Igor Mishakin <misakinigor@gmail.com>
2022-11-29 16:51:38 +0300

    feat(front): test commit

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -12,7 +12,7 @@ function IndexPage() {
         <Flex className="glow" alignItems="center" justifyContent="center" flexDirection="column">
             <ErrorBoundary>
                 <Image src="/images/svg/logo.svg" width={100} height={100} alt="logo" />
-                <div>NextJS Starter</div>
+                <div>webpraktik Starter</div>
 
                 {!isLoading && !isError && <div className="result">{data.result}</div>}
             </ErrorBoundary>
