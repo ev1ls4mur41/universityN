5dc1ae5ce2b4a22d64dcfc25de1c6d127872674f
dm1try <shrikelionel0@gmail.com>
2022-11-30 23:48:53 +0300

    feat(back): Добавлена тестовая миграция. В connection добавлена бд laravel

--- back/vendor/composer/autoload_static.php
+++ back/vendor/composer/autoload_static.php
@@ -340,8 +340,8 @@ class ComposerStaticInitf7324fd4826068e18f179c5569a28666
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-message/src',
-            1 => __DIR__ . '/..' . '/psr/http-factory/src',
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
@@ -508,6 +508,8 @@ class ComposerStaticInitf7324fd4826068e18f179c5569a28666
         'App\\Console\\Kernel' => __DIR__ . '/../..' . '/app/Console/Kernel.php',
         'App\\Exceptions\\Handler' => __DIR__ . '/../..' . '/app/Exceptions/Handler.php',
         'App\\Http\\Controllers\\Controller' => __DIR__ . '/../..' . '/app/Http/Controllers/Controller.php',
+        'App\\Http\\Controllers\\TestController' => __DIR__ . '/../..' . '/app/Http/Controllers/TestController.php',
+        'App\\Http\\Controllers\\test_controller' => __DIR__ . '/../..' . '/app/Http/Controllers/test_controller.php',
         'App\\Http\\Kernel' => __DIR__ . '/../..' . '/app/Http/Kernel.php',
         'App\\Http\\Middleware\\Authenticate' => __DIR__ . '/../..' . '/app/Http/Middleware/Authenticate.php',
         'App\\Http\\Middleware\\EncryptCookies' => __DIR__ . '/../..' . '/app/Http/Middleware/EncryptCookies.php',
