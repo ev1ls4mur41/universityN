13e26d881eee9d99e0a2aae3bb051fc0fda97ed1
dm1try <shrikelionel0@gmail.com>
2022-12-02 14:20:07 +0300

    fix(back):Вернулся к начальному варианту, сделал модель и миграцию (заглушки/прототипы)

--- back/database/factories/UserFactory.php
+++ back/database/factories/UserFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+/*namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
@@ -8,14 +8,14 @@ use Illuminate\Support\Str;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+/*class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
-    public function definition()
+  /*  public function definition()
     {
         return [
             'userName' => fake()->name(),
@@ -25,25 +25,6 @@ class UserFactory extends Factory
             'userPass' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
 
-
-
-            /*
-             *
-             *   $table->id('userId');
-            $table->string('userName');
-            $table->string('userLog')->unique();
-           // $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table -> string('userGroup');
-            $table -> boolean('isTeacher');
-            $table->rememberToken();
-            $table->timestamps();
-             *
-             *
-             *
-             *
-             *
-             */
         ];
     }
 
@@ -52,7 +33,7 @@ class UserFactory extends Factory
      *
      * @return static
      */
-    public function unverified()
+   /* public function unverified()
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -15,27 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(10)->create();
+       /* \App\Models\UserVoidModel::factory(10)->create();
 
-         \App\Models\User::factory()->create([
-             'userName' => 'Test User',
-             'userLog' => 'test@example.com',
-             'userGroup' => 'CS-50',
-             'userPass' => 'H0noRAnDG10ry'
+        \App\Models\UserVoidModel::factory()->create([
+            'userName' => 'Test User',
+            'userLog' => 'test@example.com',
+            'userGroup' => 'CS-50',
+            'userPass' => 'H0noRAnDG10ry',
 
-             /*
-              *  'userName' => fake()->name(),
-            'userLog' => fake()->unique()->safeEmail(),
-            //'email_verified_at' => now(),
-            'userGroup' => fake() -> bloodGroup(),
-            'userPass' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-              *
-              *
-              *
-              *
-              *
-              */
-         ]);
+
+            /*    DB::table('usersVoid')->insert([
+                    'name' => Str::random(10),
+                    'userLog' => Str::random(10).'@gmail.com',
+                    'userPass' => Hash::make('password'),
+                    'userGroup' => Str::random(4)."41",
+                    'remember_token' => Str::random(10),*/
+       // ]);
     }
 }
