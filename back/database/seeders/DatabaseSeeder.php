bcac19287ba8f7e3bf91827a952a192c991f88ce
dm1try <shrikelionel0@gmail.com>
2022-12-07 12:27:27 +0300

    fix(back): Откатился к "пустому" проекту, написал  корректные модель, фабрику и сидер

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserVoid1Seeder::class);
+        $this -> call(User1::class);
+
     }
 }
