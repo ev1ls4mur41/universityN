36fa89b362b991ef2a497c7db42b59c83260b776
dm1try <shrikelionel0@gmail.com>
2022-12-16 13:39:17 +0300

    feat(back):Добавил апи на постзапрос контакты.

--- back/app/Http/Controllers/ContactsController.php
+++ back/app/Http/Controllers/ContactsController.php
@@ -8,43 +8,61 @@ use Illuminate\Http\Request;
 class ContactsController extends Controller
 {
     /**
+     * @return \Illuminate\Http\JsonResponse
+     *
+     *
      * @OA\Get(
      *path="/contacts",
      *operationId = "getContacts",
      *tags={"contacts"},
      *summary="Get Contacts",
-     *     @OA\Response(
+     *@OA\Response(
      *response = "200",
      *description ="OK"
      * )
      *)
-     * */
+     */
     protected function getContacts()
     {
         $array = [
             'firstName' => 'User',
             'lastName' => 'Name',
+            'middleName' => '',
             'userLogin' => 'QweRty@asd.zxc',
             'numberDiary' => '1234/123',
             'userGroup' => 'CS50',
             'phoneNumber' => '78889994561',
-            'isTeacher' => '0',
-            'isAdmin' => '0',
             'isLeader' => '1',
             'imgLink' => 'ibb.co/qasdxc',
             'averageMark' => '4.0',
-            'userPass' => '4.0'
         ];
         return response()->json($array, 200);
     }
 
+    /**
+     * @return \Illuminate\Http\JsonResponse
+     *
+     * @OA\Post(
+     *     path="/registr",
+     *     operationId="regUser",
+     *     tags={"Users"},
+     *     summary="Registration user",
+     *     @OA\Response(
+     *          response="201",
+     *          description="Successful operation"
+     *     )
+     * )
+     *
+     *
+     */
     protected function postContacts()
     {
         $arr = [
-            'notice' => 'CREATED',
+            'notice' => 'CREATED'
         ];
         return response()->json($arr, 201);
     }
+
 
     protected function deleteContacts()
     {
